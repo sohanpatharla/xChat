2f675e26288c139ae3af114786c23805a013ea65
sohanpatharla <117771862+sohanpatharla@users.noreply.github.com>
2024-11-24 04:20:31 +0530

    Fix: Corrected the Action Messages in frontend and backend dirs

--- Backend/Actions.js
+++ Backend/Actions.js
@@ -8,12 +8,37 @@ const ACTIONS = {
     SEND_MESSAGE: 'send-message',
     RECEIVE_MESSAGE: 'receive-message',
     TOGGLE_EDITOR_LOCK: 'TOGGLE_EDITOR_LOCK',
-    NEXT_CHAT:'next-chat',
+    //NEXT_CHAT:'next-chat',
     USER_CHANGES: 'user-changes',
     MATCH_USERS:'match-users',
-    NAVIGATE_CHAT:'navigate-to-chat',
+    NEXT_CHAT: "next_chat",
+    NAVIGATE_CHAT: "navigate_chat",
+    //NAVIGATE_CHAT:'navigate-to-chat',
     
 
 };
-
+// const ACTIONS = {
+//     JOIN: "join",
+//     JOINED: "joined",
+//     DISCONNECTED: "disconnected",
+//     NEXT_CHAT: "next_chat",
+//     NAVIGATE_CHAT: "navigate_chat",
+//   };
 module.exports = ACTIONS;
+// const ACTIONS = {
+//     JOIN: 'join',
+//     JOINED: 'joined',
+//     DISCONNECTED: 'disconnected',
+//     CODE_CHANGE: 'code-change',
+//     SYNC_CODE: 'sync-code',
+//     LEAVE: 'leave',
+//     SEND_MESSAGE: 'send-message',
+//     RECEIVE_MESSAGE: 'receive-message',
+//     TOGGLE_EDITOR_LOCK: 'TOGGLE_EDITOR_LOCK',
+//     USER_CHANGES: 'user-changes',
+//     MATCH_USERS: 'match-users',
+//     NEXT_CHAT: 'next_chat',
+//     NAVIGATE_CHAT: 'navigate_chat',
+// };
+
+// module.exports = ACTIONS;
--- Frontend/src/Actions.js
+++ Frontend/src/Actions.js
@@ -10,8 +10,35 @@ const ACTIONS = {
     TOGGLE_EDITOR_LOCK: 'TOGGLE_EDITOR_LOCK',
     USER_CHANGES: 'user-changes',
     MATCH_USERS:'match-users',
-    NAVIGATE_CHAT:'navigate-to-chat',
+    NEXT_CHAT: "next_chat",
+    NAVIGATE_CHAT: "navigate_chat",
 
 };
+// const ACTIONS = {
+//     JOIN: "join",
+//     JOINED: "joined",
+//     DISCONNECTED: "disconnected",
+//     NEXT_CHAT: "next_chat",
+//     NAVIGATE_CHAT: "navigate_chat",
+//   };
+//module.exports = ACTIONS;
+  
 
 module.exports = ACTIONS;
+// const ACTIONS = {
+//   JOIN: 'join',
+//   JOINED: 'joined',
+//   DISCONNECTED: 'disconnected',
+//   CODE_CHANGE: 'code-change',
+//   SYNC_CODE: 'sync-code',
+//   LEAVE: 'leave',
+//   SEND_MESSAGE: 'send-message',
+//   RECEIVE_MESSAGE: 'receive-message',
+//   TOGGLE_EDITOR_LOCK: 'TOGGLE_EDITOR_LOCK',
+//   USER_CHANGES: 'user-changes',
+//   MATCH_USERS: 'match-users',
+//   NEXT_CHAT: 'next_chat',
+//   NAVIGATE_CHAT: 'navigate_chat',
+// };
+
+// module.exports = ACTIONS;
